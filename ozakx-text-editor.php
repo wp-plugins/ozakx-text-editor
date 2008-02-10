9c5bc391116b6a53378eceeeb640230ab70f9437
anuragbhateja <anuragbhateja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-10 10:02:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ozakx-text-editor/tags/1.0@31311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ozakx-text-editor.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Ozakx Text Editor
+Plugin URI: http://www.anuragbhateja.com/wordpress/ozakx-text-editor.html
+Description: With this plugin, you can add link, tooltip, style to any word in the post/page/comment
+Version: 1.0
+Author: Anurag Bhateja
+Author URI: http://www.anuragbhateja.com
+*/ 
+
+
+function ozakx_link_editor($text) {
+/* 
+To add link use the following syntex
+        $text = preg_replace('"Word/Phrase"','<b><a title="Title Comes Here" target="_blank" href="http://www.yourdomain.com/link,html">Word/Phrase Again</a></b>',$text);
+
+To add Bold with tooltip use the following syntex
+        $text = preg_replace('"Word/Phrase"','<b title="Tootip title">Word/Phrase Again</b>',$text);
+
+To add Italic with tooltip use the following syntex
+        $text = preg_replace('"Word/Phrase"','<i title="Tootip title">Word/Phrase Again</i>',$text);
+
+To add Underline with tooltip use the following syntex
+        $text = preg_replace('"Word/Phrase"','<u title="Tootip title">Word/Phrase Again</u>',$text);
+
+You can also use combinations like of bold with italic and underline. A little HTML knowledge can make you control better.
+*/
+
+
+        $text = preg_replace('"Ozakx Text Editor"','<u title="Wordpress Plugin Provided By Ozakx Technologies - Developed By Anurag Bhateja">Ozakx Text Editor</u>',$text);
+
+    return $text;
+
+}
+
+// Highlight text and comments:
+add_filter('the_content', 'ozakx_link_editor');
+add_filter('comment_text', 'ozakx_link_editor');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+Presenting a plugin which will help you to create links/give styles/tooltips in the whole Wordpress Blog.
+
+Name: Ozakx Text Editor
+
+Version: 1.0
+
+Author Name: Anurag Bhateja
+
+Author URL: http://www.anuragbhateja.com
+
+Plugin URL: http://www.anuragbhateja.com/wordpress/ozakx-text-editor.html
+
+Remember:
+
+1. It is case sensitive. It is made like this to Avoid confliction with image names in post. If you have added image with name ipod.jpg and make link in post like with Ipod or iPod the image link will not get disturbed.
+2. A little knowledge of HTML is required.
+3. Please report if find any bugs.
+
+Link: OZAKX Text Editor >> Right Click >> Save Target As
+
+To use >> Upload ozakx-text-editor.php to you wp-content/plugins folder >> Go to Admin section >> Plugins >> Activate
+
+To Add/Edit Links >> Admin sections >> Plugins >> Plugin Editor >> “Ozakx Text Editor” and follow the instructions given in it.
